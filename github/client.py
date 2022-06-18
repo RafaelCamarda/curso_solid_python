0ea9c95f732156ec3865c7aba2efdf838eaf33bd
Mikaelson Braz <57190676+mikaelsonbraz@users.noreply.github.com>
2022-06-18 13:59:17 -0300

    Update client.py

--- github/client.py
+++ github/client.py
@@ -10,7 +10,5 @@ class GithubCliient():
     def get_repos_by_user(self, user):
         response = requests.get(
             f'{self.API_BASE_URL}/users/{user}/repos')
-        if response.status_code == 200:
-            return {"status_code": 200, "body": response.json()}
-        else:
-            return {"status_code": response.status_code, "body": "Error while getting repos"}
+        return {"status_code": response.status_code,
+                "body": response.json() if response.status_code == 200 else "Error while getting repos"}
